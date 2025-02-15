ec65f0be68007d4f7ddf31ad6cba2054715e2e5e
CodeByToby <163883132+CodeByToby@users.noreply.github.com>
2025-02-15 01:04:36 +0100

    MINOR: Error handling and early continue for fork

--- Server/server.c
+++ Server/server.c
@@ -15,7 +15,7 @@
 int main (int argc, char** argv) 
 {
     int nSent, nRead;
-
+    int retval;
     int sockfd,
         sockfd_accpt;
     const struct sockaddr_un addr = { AF_UNIX, "/tmp/ftp.sock" };
@@ -71,90 +71,99 @@ int main (int argc, char** argv)
 
         // FORK LOOP
 
-        if (fork() == 0) {
-            while (TRUE) {
-                nRead = recv(sockfd_accpt, (command_t*) &cmd, sizeof(command_t), 0);
+        retval = fork()
 
-                if (nRead < 0) {
-                    fprintf(stderr, "<ERR> PID_%d: ", getpid());
-                    perror("recv()");
+        if (retval < 0) {
+            fprintf(stderr, "<ERR> ");
+            perror("fork()"); 
 
-                    close(sockfd);
-                    exit(EXIT_FAILURE);
-                }
+            close(sockfd_accpt);
+        } else if (retval > 0) {
+            continue;
+        }
 
-                switch (cmd.type) {
-                case LIST:
-                    ftp_list(sockfd_accpt, &res);
-                    printf("[CLIENT_%d] LIST\n", getpid());
-                    break;
+        while (TRUE) {
+            nRead = recv(sockfd_accpt, (command_t*) &cmd, sizeof(command_t), 0);
 
-                case RETR:
-                    //ftp_retr(sockfd_accpt, &res);
-                    printf("[CLIENT_%d] RETR\n", getpid());
-                    break;
+            if (nRead < 0) {
+                fprintf(stderr, "<ERR> PID_%d: ", getpid());
+                perror("recv()");
 
-                case STOR:
-                    //ftp_stor(sockfd_accpt, &res);
-                    printf("[CLIENT_%d] STOR\n", getpid());
-                    break;
+                close(sockfd_accpt);
+                exit(EXIT_FAILURE);
+            }
 
-                case STOU:
-                    //ftp_stou(sockfd_accpt, &res);
-                    printf("[CLIENT_%d] STOU\n", getpid());
-                    break;
+            switch (cmd.type) {
+            case LIST:
+                ftp_list(sockfd_accpt, &res);
+                printf("[CLIENT_%d] LIST\n", getpid());
+                break;
 
-                case APPE:
-                    //ftp_appe(sockfd_accpt, &res);
-                    printf("[CLIENT_%d] APPE\n", getpid());
-                    break;
+            case RETR:
+                //ftp_retr(sockfd_accpt, &res);
+                printf("[CLIENT_%d] RETR\n", getpid());
+                break;
 
-                case DELE:
-                    //ftp_dele(sockfd_accpt, &res);
-                    printf("[CLIENT_%d] DELE\n", getpid());
-                    break;
+            case STOR:
+                //ftp_stor(sockfd_accpt, &res);
+                printf("[CLIENT_%d] STOR\n", getpid());
+                break;
 
-                case RMD:
-                    //ftp_rmd(sockfd_accpt, &res);
-                    printf("[CLIENT_%d] RMD\n", getpid());
-                    break;
+            case STOU:
+                //ftp_stou(sockfd_accpt, &res);
+                printf("[CLIENT_%d] STOU\n", getpid());
+                break;
 
-                case MKD:
-                    //ftp_mkd(sockfd_accpt, &res);
-                    printf("[CLIENT_%d] MKD\n", getpid());
-                    break;
+            case APPE:
+                //ftp_appe(sockfd_accpt, &res);
+                printf("[CLIENT_%d] APPE\n", getpid());
+                break;
 
-                case PWD:
-                    //ftp_pwd(sockfd_accpt, &res);
-                    printf("[CLIENT_%d] PWD\n", getpid());
-                    break;
+            case DELE:
+                //ftp_dele(sockfd_accpt, &res);
+                printf("[CLIENT_%d] DELE\n", getpid());
+                break;
 
-                case CWD:
-                    //ftp_cwd(sockfd_accpt, &res);
-                    printf("[CLIENT_%d] CWD\n", getpid());
-                    break;
+            case RMD:
+                //ftp_rmd(sockfd_accpt, &res);
+                printf("[CLIENT_%d] RMD\n", getpid());
+                break;
 
-                case CDUP:
-                    //ftp_cdup(sockfd_accpt, &res);
-                    printf("[CLIENT_%d] CDUP\n", getpid());
-                    break;
+            case MKD:
+                //ftp_mkd(sockfd_accpt, &res);
+                printf("[CLIENT_%d] MKD\n", getpid());
+                break;
 
-                case NOOP:
-                    ftp_noop(sockfd_accpt, &res);
-                    printf("[CLIENT_%d] NOOP\n", getpid());
-                    break;
+            case PWD:
+                //ftp_pwd(sockfd_accpt, &res);
+                printf("[CLIENT_%d] PWD\n", getpid());
+                break;
 
-                case QUIT:
-                    //ftp_quit(sockfd_accpt, &res);
-                    printf("[CLIENT_%d] QUIT\n", getpid());
-                    break;
+            case CWD:
+                //ftp_cwd(sockfd_accpt, &res);
+                printf("[CLIENT_%d] CWD\n", getpid());
+                break;
 
-                case HELP:
-                default:
-                    //ftp_help(sockfd_accpt, &res);
-                    printf("[CLIENT_%d] HELP\n", getpid());
-                    break;
-                }
+            case CDUP:
+                //ftp_cdup(sockfd_accpt, &res);
+                printf("[CLIENT_%d] CDUP\n", getpid());
+                break;
+
+            case NOOP:
+                ftp_noop(sockfd_accpt, &res);
+                printf("[CLIENT_%d] NOOP\n", getpid());
+                break;
+
+            case QUIT:
+                //ftp_quit(sockfd_accpt, &res);
+                printf("[CLIENT_%d] QUIT\n", getpid());
+                break;
+
+            case HELP:
+            default:
+                //ftp_help(sockfd_accpt, &res);
+                printf("[CLIENT_%d] HELP\n", getpid());
+                break;
             }
         }
     }
