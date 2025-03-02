d8dd8ed2d85fca80da547ba3143bd6b09118a560
CodeByToby <163883132+CodeByToby@users.noreply.github.com>
2025-03-02 22:46:23 +0100

    FUNCTION_DATA_CONN: PASV command – done client-side

--- Client/client.c
+++ Client/client.c
@@ -17,7 +17,22 @@
 #define CONN_PORT "3456"
 
 static int conn_socket_create(char * hostname);
+static int data_pasv_socket_create(char * hostname, char * port);
 static int command_get(command_t * cmd);
+
+typedef struct ipv4_addr {
+    int p1;
+    int p2;
+    int p3;
+    int p4;
+} ipv4_addr_t;
+
+typedef struct port {
+    int p1;
+    int p2;
+} port_t;
+
+int sockfd_data = 0;
 
 int main(int argc, char** argv)
 {
@@ -72,9 +87,37 @@ int main(int argc, char** argv)
         // TODO: Print reply
         printf("%d, %s\n", res.code, res.message);
 
+        switch (cmd.type) {
+        case PASV:
+            if (res.code != 227)
+                break;
+
+            ipv4_addr_t ipv4;
+            port_t data_port;
+
+            char ipv4_str[100];
+            char data_port_str[10];
+
+            sscanf(res.message, "Entering passive mode. %d,%d,%d,%d,%d,%d",
+                &ipv4.p1, &ipv4.p2, &ipv4.p3, &ipv4.p4, &data_port.p1, &data_port.p2);
+
+            snprintf(ipv4_str, 100, "%d.%d.%d.%d", ipv4.p1, ipv4.p2, ipv4.p3, ipv4.p4);
+            snprintf(data_port_str, 10, "%d", data_port.p1 * 256 + data_port.p2);
+
+            printf("%s:%s\n", ipv4_str, data_port_str);
+
+            sockfd_data = data_pasv_socket_create(ipv4_str, data_port_str);
+
+            break;
+            
+        case LIST:
+            break;
+        }
+
         // Quit
         if (res.code == 221 || res.code == 421) {
             close(sockfd);
+            close(sockfd_data);
             exit(EXIT_SUCCESS);
         }
     }
@@ -214,7 +257,57 @@ static int conn_socket_create(char * hostname) {
     struct sockaddr_in *ip = (struct sockaddr_in *) p->ai_addr;
 
     inet_ntop(p->ai_family, &(ip->sin_addr), ipstr, sizeof(ipstr));
-    printf("Server running on %s:%s...\n", ipstr, CONN_PORT);
+    printf("Server connected on %s:%s...\n", ipstr, CONN_PORT);
+
+    freeaddrinfo(res);
+
+    return sockfd;
+}
+
+static int data_pasv_socket_create(char * hostname, char * port) {
+    int sockfd;
+    struct addrinfo hints, * res, * p;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET; // IPv4
+    hints.ai_socktype = SOCK_STREAM; // TCP
+
+    if (getaddrinfo(hostname, port, &hints, &res) != 0) {
+        perror("getaddrinfo()");
+        exit(EXIT_FAILURE);
+    }
+
+    for(p = res; p != NULL; p = p->ai_next) {
+
+        // CREATE AN ENDPOINT
+
+        if((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) < 0) { 
+            perror("socket()"); 
+            continue;
+        }
+
+        // CONNECT TO THE SOCKET
+
+        if(connect(sockfd, p->ai_addr, p->ai_addrlen) < 0) { 
+            perror("connect()"); 
+            close(sockfd);
+            continue;
+        }
+
+        break;
+    }
+
+    if (p == NULL) {
+        fprintf(stderr, "Client failed to connect\n");
+        freeaddrinfo(res);
+        exit(EXIT_FAILURE);
+    }
+
+    char ipstr[INET_ADDRSTRLEN];
+    struct sockaddr_in *ip = (struct sockaddr_in *) p->ai_addr;
+
+    inet_ntop(p->ai_family, &(ip->sin_addr), ipstr, sizeof(ipstr));
+    printf("Server connected on %s:%s...\n", ipstr, CONN_PORT);
 
     freeaddrinfo(res);
 
